6c0cec4727c592814509ee2b83dff86786c94b43
Nassim Kirouane <nassim.kirouane@matters.tech>
2019-02-24 11:24:25 +0100

    jest

---
+++ src/math.test.js
@@ -0,0 +1,23 @@
+const Util = require('./math');
+
+describe('Factorial', function () {
+
+    test.each([
+        [0, 1],
+        [1, 1],
+        [2, 2],
+        [3, 6],
+        [4, 24],
+        [5, 120],
+    ])(
+        'Factorial %i equals to %i',
+        (n, expected) => {
+            expect(Util.factorial(n)).toBe(expected);
+        },
+    );
+
+    test('Factorial of negative integers throws exception', () => {
+        expect(()=> {Util.factorial(-10)}).toThrow();
+    });
+});
+
