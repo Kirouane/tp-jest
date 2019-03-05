3282cbd7500c6066a8a300a45ef682eb871be9d9
Nassim Kirouane <cgmpnk@gmail.com>
2019-03-05 14:44:42 +0100

    is prime

--- src/math.test.js
+++ src/math.test.js
@@ -18,3 +18,31 @@ test('Test factoriel de 3000', () => {
 test('Test factoriel -10', () => {
     expect(()=> {Util.factorial(-10)}).toThrow(/negative/);
 });
+
+
+describe('isPrime', function () {
+
+    test('Test prime de 1 => false', () => {
+        expect(Util.isPrime(1)).toBe(false)
+    });
+    test('Test prime de 0 => false', () => {
+        expect(Util.isPrime(0)).toBe(false)
+    });
+    test('Test prime < 0 => throw exception', () => {
+        expect(() => { Util.isPrime(-10) }).toThrow('Unable to compute prime for n < 0');
+    });
+
+    test.each([
+        [2, true],
+        [5, true],
+        [17, true],
+        [18, false],
+        [53, true],
+        [55, false],
+    ])(
+        'isPrime %i equals to %i',
+        (n, expected) => {
+            expect(Util.isPrime(n)).toBe(expected);
+        }
+    );
+});
